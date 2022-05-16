d1bf98363d12fe178888f338116d214e1c8ddc29
Stefanos Chalkidis <s.chalkidis@protonmail.com>
2022-05-16 13:34:40 +0300

    added private constructor

--- client-lib/src/main/java/com/signnow/library/Constants.java
+++ client-lib/src/main/java/com/signnow/library/Constants.java
@@ -3,4 +3,8 @@ package com.signnow.library;
 public class Constants {
     public static String AUTHORIZATION = "Authorization";
     public static String BEARER = "Bearer ";
+
+    private Constants() {
+        throw new IllegalStateException("Constants class");
+    }
 }
