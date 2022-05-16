ec1306f121bf12a699b9e170bbe2625df5046e7a
Stefanos Chalkidis <s.chalkidis@protonmail.com>
2022-05-16 17:35:32 +0300

    removed redundant import

--- client-lib/src/main/java/com/signnow/library/dto/GenericId.java
+++ client-lib/src/main/java/com/signnow/library/dto/GenericId.java
@@ -3,7 +3,6 @@ package com.signnow.library.dto;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Collections;
 import java.util.HashMap;
