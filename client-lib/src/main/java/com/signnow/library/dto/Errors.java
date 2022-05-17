1ce8a53d3dc6a028af142f1bbf053c26c4d38a2a
Stefanos Chalkidis <s.chalkidis@protonmail.com>
2022-05-17 12:54:42 +0300

    added @SuppressWarnings

--- client-lib/src/main/java/com/signnow/library/dto/Errors.java
+++ client-lib/src/main/java/com/signnow/library/dto/Errors.java
@@ -2,6 +2,7 @@ package com.signnow.library.dto;
 
 import java.util.List;
 
+@SuppressWarnings("java:S1104")  // field name equal to JsonProperty
 public class Errors {
     public List<ErrorInfo> errorList;
 
